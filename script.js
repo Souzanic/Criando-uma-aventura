a9d6e4a190ee3bcd45ae0475f766b22926d10664
Souzanic <o.nicolly17@escola.pr.gov.br>
2025-09-05 09:38:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Seleciona todos os botões
+const botoes = document.querySelectorAll(".btn-proximo");
 
+botoes.forEach(botao => {
+  botao.addEventListener("click", () => {
+    const proximo = botao.getAttribute("data-proximo");
+
+    // esconde o passo atual
+    const passoAtual = botao.parentElement;
+    passoAtual.classList.remove("ativo");
+
+    // mostra o próximo
+    const passoProximo = document.getElementById("passo-" + proximo);
+    passoProximo.classList.add("ativo");
+  });
+});
